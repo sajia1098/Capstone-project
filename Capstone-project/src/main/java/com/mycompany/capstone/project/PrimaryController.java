c504280a9487f990fc85a22059073d793f97f9e1
Elan Cheung <elan.cheung@gmail.com>
2023-10-08 14:51:14 -0400

    Test merge

--- Capstone-project/src/main/java/com/mycompany/capstone/project/PrimaryController.java
+++ Capstone-project/src/main/java/com/mycompany/capstone/project/PrimaryController.java
@@ -9,5 +9,6 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
         System.out.println("Test");
+        System.out.println("TEST MERGE");
     }
 }
