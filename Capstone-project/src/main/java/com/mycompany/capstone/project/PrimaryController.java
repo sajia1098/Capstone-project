cda55d5f42eea68039da8bf2ba5affcfe32cced5
Elan Cheung <elan.cheung@gmail.com>
2023-10-15 20:07:32 -0400

    Current LOGIN ui

--- Capstone-project/src/main/java/com/mycompany/capstone/project/PrimaryController.java
+++ Capstone-project/src/main/java/com/mycompany/capstone/project/PrimaryController.java
@@ -2,10 +2,13 @@ package com.mycompany.capstone.project;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
 
     @FXML
+    private Button bnNeedAccount;
+
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
         System.out.println("Test");
