b7a9e919b7a18c6ed6e9b8fe2593c9367c471414
[Amna Sajid] <sajia@farmingdale.edu>
2023-10-29 01:33:29 -0400

    database working commit

--- Capstone-project/src/main/java/com/mycompany/capstone/project/PrimaryController.java
+++ Capstone-project/src/main/java/com/mycompany/capstone/project/PrimaryController.java
@@ -9,6 +9,7 @@ public class PrimaryController {
     @FXML
     private Button bnNeedAccount;
 
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
         
