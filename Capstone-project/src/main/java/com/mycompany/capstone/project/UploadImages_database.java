4b92c2b7581bc16bb163072c48fcf80fcd180721
Elan Cheung <elan.cheung@gmail.com>
2023-11-09 12:26:20 -0500

    Added show all images from database button

--- Capstone-project/src/main/java/com/mycompany/capstone/project/UploadImages_database.java
+++ Capstone-project/src/main/java/com/mycompany/capstone/project/UploadImages_database.java
@@ -5,6 +5,7 @@
 package com.mycompany.capstone.project;
 
 import com.google.api.core.ApiFuture;
+import com.google.api.gax.paging.Page;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.WriteResult;
 import javafx.scene.control.TextField;
@@ -35,6 +36,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
+import javafx.event.ActionEvent;
 
 /**
  *
@@ -97,8 +99,8 @@ public class UploadImages_database {
             Blob blob = storage.create(blobInfo, Files.readAllBytes(file.toPath()));
 
             // Generate a signed URL for the blob with a long expiration time
-        URL signedUrl = storage.signUrl(blobInfo, 7, TimeUnit.DAYS, Storage.SignUrlOption.withV4Signature());
-            
+            URL signedUrl = storage.signUrl(blobInfo, 7, TimeUnit.DAYS, Storage.SignUrlOption.withV4Signature());
+
             //use storageclient to get download URL
             String imageUrl = signedUrl.toString();
 
@@ -116,6 +118,35 @@ public class UploadImages_database {
         } catch (Exception e) {
             System.err.println("Exception occurred while uploading file to Firebase Storage: " + e.getMessage());
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void showImage(ActionEvent event) {
+        flowPane.getChildren().clear();
+        // Get the Firebase Storage bucket
+        FirebaseApp firebaseApp = FirebaseApp.getInstance();
+        Storage storage = StorageClient.getInstance(firebaseApp).bucket("csc325-capstone.appspot.com").getStorage();
+
+        // List all the blobs in the bucket (this is your images)
+        Page<Blob> blobs = storage.list("csc325-capstone.appspot.com", Storage.BlobListOption.pageSize(100));
+        for (Blob blob : blobs.iterateAll()) {
+            // For each blob (image), generate a signed URL
+            BlobId blobId = blob.getBlobId();
+            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+            URL signedUrl = storage.signUrl(blobInfo, 7, TimeUnit.DAYS, Storage.SignUrlOption.withV4Signature());
+
+            // Create an image view and add it to the flow pane
+            String imageUrl = signedUrl.toString();
+            javafx.application.Platform.runLater(() -> {
+                Image image = new Image(imageUrl);
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(250);
+                imageView.setPreserveRatio(true);
+                Label descriptionLabel = new Label(blob.getName()); // Use the blob name as the description or any other metadata you have
+                VBox imageAndDescription = new VBox(imageView, descriptionLabel);
+                flowPane.getChildren().add(imageAndDescription);
+            });
         }
     }
 }
