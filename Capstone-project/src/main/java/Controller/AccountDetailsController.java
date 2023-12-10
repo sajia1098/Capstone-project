9590b3986b95fffdc7c515c9443cab02b474a686
Robert Biersach <robertbiersach01@gmail.com>
2023-12-10 16:44:56 -0500

    working account details

--- Capstone-project/src/main/java/Controller/AccountDetailsController.java
+++ Capstone-project/src/main/java/Controller/AccountDetailsController.java
@@ -4,24 +4,29 @@
  */
 package Controller;
 import Model.CurrentUser;
+import Model.Item;
+import Model.ItemDetails;
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.cloud.FirestoreClient;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 //import model.DisplayItem;
@@ -35,7 +40,7 @@ public class AccountDetailsController{
     private Button changeProfilePictureButton;
 
     @FXML
-    private GridPane itemContainer;
+    private FlowPane flowPane;
 
     @FXML
     private Circle profilePictureCircle;
@@ -72,7 +77,10 @@ public class AccountDetailsController{
         App.setRoot("home");
 
     }
-    
+        public void initialize() {
+        fetchUserDetails();
+            showImagesByCategory(CurrentUser.getInstance().getId());
+    }
 private void fetchUserDetails() {
         String currentUserId = CurrentUser.getInstance().getId();
         Firestore db = FirestoreClient.getFirestore();
@@ -99,10 +107,81 @@ private void fetchUserDetails() {
             }
         }, Executors.newSingleThreadExecutor());
     }
-        
-    public void initialize() {
-        fetchUserDetails();
-    }
-    
 
+private void showImagesByCategory(String category) {
+        flowPane.getChildren().clear();
+
+        Task<Void> loadCategoryImagesTask = new Task<Void>() {
+            @Override
+            protected Void call() {
+                try {
+                    Firestore db = FirestoreClient.getFirestore();
+                    CollectionReference items = db.collection("Items");
+
+                    //Query items by category
+                    Query query = items.whereEqualTo("RamID", category);
+                    ApiFuture<QuerySnapshot> querySnapshot = query.get();
+
+                    for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
+                        Map<String, Object> item = document.getData();
+
+                        //imageUrl and other metadata are stored in the document
+                        String imageUrl = (String) item.get("imageUrl");
+                        String productName = (String) item.get("productName");
+                        String productPrice = String.format("%.2f", (Double) item.get("price"));
+                        String condition = (String) item.get("condition");
+                        String category = (String) item.get("category");
+                        String comments = (String) item.get("comments");
+                        String description = (String) item.get("description");
+
+                        Image image = new Image(imageUrl);
+                        Platform.runLater(() -> {
+                            ImageView imageView = new ImageView(image);
+                            imageView.setFitWidth(200);
+                            imageView.setPreserveRatio(true);
+
+                            imageView.setOnMouseClicked(event -> {
+                                double priceValue = Double.parseDouble(productPrice);
+                                Item itemClass = new Item(category, comments, condition, description, priceValue, imageUrl, productName);
+                                openItemDetail(itemClass);
+                            });
+
+                            Label nameLabel = new Label("Name: " + productName);
+                            Label priceLabel = new Label("Price: $" + productPrice);
+                            Label conditionLabel = new Label("Condition: " + condition);
+                            Label categoryLabel = new Label("Category: " + category);
+                            VBox imageAndDescription = new VBox(imageView, nameLabel, priceLabel, conditionLabel, categoryLabel);
+                            addItemToFlowPane(imageAndDescription);
+                        });
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+        Thread thread = new Thread(loadCategoryImagesTask);
+        thread.setDaemon(true);
+        thread.start();
+    }
+        
+
+    private void openItemDetail(Item itemDetails) {
+        try {
+            //Set item details in ItemDetails class
+            ItemDetails.getInstance().setCurrentItem(itemDetails);
+
+            //Use App class to switch scenes
+            App.switchScene("itemdescription");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        private synchronized void addItemToFlowPane(VBox itemBox) {
+        Platform.runLater(() -> {
+            flowPane.getChildren().add(itemBox);
+        });
+    }
 }
+    
