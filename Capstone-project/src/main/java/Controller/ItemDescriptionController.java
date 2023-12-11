d8c7fb9e6dbb56e2ec5589f3c0842c2e0c5fdaa2
Elan Cheung <elan.cheung@gmail.com>
2023-12-10 23:38:07 -0500

    merged conflicts

--- Capstone-project/src/main/java/Controller/ItemDescriptionController.java
+++ Capstone-project/src/main/java/Controller/ItemDescriptionController.java
@@ -10,23 +10,18 @@ import Model.SharedResource;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 import project.App;
 
 /**
  * FXML Controller class
  *
- * @author amnasajid
+ * @author elan
  */
 public class ItemDescriptionController implements Initializable {
 
